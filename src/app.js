aec172b73536f82dbd315354078a6d2d979ab045
unknown <sasee9488@gmail.com>
2019-12-24 00:38:47 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
 //Access Port
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
+//db connection
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true,useUnifiedTopology: true }).then(() => {
+  console.log("DB Connected");
+});
+//db connection Error handling
+mongoose.connection.on("error", err => {
+  console.log(`DB Connection Error:${err.message}`);
+});
 //bring routes
 const postRoute = require("../routes/post");
 
@@ -14,7 +25,7 @@ const myOwnMiddleware = (req, res, next) => {
 //middleware
 app.use(morgan("dev"));
 app.use(myOwnMiddleware);
-app.use("/", postRoute);
+app.use(postRoute);
 
 app.listen(PORT, err => {
   if (err) return console.log(`Cannot Listen on PORT: ${PORT}`);
